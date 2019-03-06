8e9f25d059ab12bca342e31ec06327e611dad483
Denis Chernykh <denis_chernykh@epam.com>
2019-03-06 19:29:25 +0300

    add logging

--- sample_test.py
+++ sample_test.py
@@ -9,13 +9,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 class TestMy:
 
     def setup_method(self):
+        print("Open google page")
         self.driver: WebDriver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get("https://www.google.ru/")
 
     def test_01hello_world(self):
+        print("Try to search in google")
         self.driver.find_element_by_name("q").send_keys("Moscow Stock Exchange Repository\n")
 
     def test_02hello_world(self):
+        print("Check results stats appeared")
         self.driver.find_element_by_id("resultStats")
         assert WebDriverWait(self.driver, 10).until(
             expected_conditions.presence_of_element_located((By.ID, "resultStats")).is_displayed())
